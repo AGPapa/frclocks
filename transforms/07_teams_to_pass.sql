fdb7f8fa2763180b14cabcbde68a7a04c532d993
Antonio Papa <antoniogpapa@gmail.com>
2025-03-11 08:54:59 -0400

    Fixes double-counting of impact award on capacity

--- transforms/07_teams_to_pass.sql
+++ transforms/07_teams_to_pass.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS teams_to_pass AS (
-    WITH num_events AS (
+    WITH num_events_remaining AS (
         SELECT
-            events.district_key,
+            event_states.district_key,
             COUNT(*) AS num_events
-        FROM events
-        WHERE events.event_type = 'District'
-        GROUP BY events.district_key
+        FROM event_states
+        WHERE event_states.event_state != 'Completed'
+        GROUP BY event_states.district_key
     )
     SELECT
         district_rankings_without_impact.district_key,
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS teams_to_pass AS (
         district_rankings_without_impact.inflated_points,
         district_rankings_without_impact.rank,
         district_rankings_without_impact.active_team_rank,
-        (district_lookup.dcmp_capacity - num_events.num_events) - district_rankings_without_impact.active_team_rank + 1 AS teams_to_pass
+        (district_lookup.dcmp_capacity - num_events_remaining.num_events) - district_rankings_without_impact.active_team_rank + 1 AS teams_to_pass
     FROM district_rankings_without_impact
     JOIN district_lookup ON district_rankings_without_impact.district_key = district_lookup.district_key
-    JOIN num_events ON district_rankings_without_impact.district_key = num_events.district_key
-    WHERE (district_lookup.dcmp_capacity - num_events.num_events) - district_rankings_without_impact.active_team_rank + 1 > 0
+    JOIN num_events_remaining ON district_rankings_without_impact.district_key = num_events_remaining.district_key
+    WHERE (district_lookup.dcmp_capacity - num_events_remaining.num_events) - district_rankings_without_impact.active_team_rank + 1 > 0
 )

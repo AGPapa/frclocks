7a164dee49a29c8c4df3035703b95965356f419f
Antonio Papa <antoniogpapa@gmail.com>
2026-04-12 22:59:43 -0400

    Fixes RAS calculation for California

--- transforms/dcmp_regions/04_wcmp_spots.sql
+++ transforms/dcmp_regions/04_wcmp_spots.sql
@@ -27,34 +27,6 @@ CREATE TABLE IF NOT EXISTS wcmp_spots AS (
             AND event_states.event_key = qualifying_award_winners.event_key
         WHERE event_states.event_type = 'District Championship'
         GROUP BY event_states.district_key, event_states.event_key
-    ),
-    dcmp_events_needing_ras AS (
-        SELECT
-            event_states.district_key,
-            event_states.event_key
-        FROM event_states
-        WHERE event_states.event_type = 'District Championship'
-            AND event_states.event_state IN ('Completed', 'Awards')
-    ),
-    dcmp_events_with_ras AS (
-        SELECT DISTINCT
-            events.district_key,
-            awards.event_key
-        FROM awards
-        JOIN events ON awards.event_key = events.event_key
-        WHERE events.event_type = 'District Championship'
-            AND awards.award_type = 10
-    ),
-    missing_qualifying_awards AS (
-        SELECT
-            dcmp_events_needing_ras.district_key,
-            COUNT(*) AS num_missing_qualifying_awards
-        FROM dcmp_events_needing_ras
-        LEFT JOIN dcmp_events_with_ras
-            ON dcmp_events_needing_ras.district_key = dcmp_events_with_ras.district_key
-            AND dcmp_events_needing_ras.event_key = dcmp_events_with_ras.event_key
-        WHERE dcmp_events_with_ras.event_key IS NULL
-        GROUP BY dcmp_events_needing_ras.district_key
     )
     SELECT
         district_lookup.district_key,
@@ -65,11 +37,10 @@ CREATE TABLE IF NOT EXISTS wcmp_spots AS (
             - SUM(COALESCE(bonus_point_spots.num_dcmp_winners, 4)) -- Assume there will be a backup team on the winning alliance unless we know otherwise. South Carolina only sends 2 winners
             + SUM(COALESCE(bonus_point_spots.num_double_qualifying_awards, 0)) -- If a winner also gets a qualifying award, then an extra point spot is available
             + 2 * SUM(COALESCE(bonus_point_spots.num_triple_qualifying_awards, 0)) -- If a prequalified also gets two qualifying awards, then an two extra point spots are available
-            + SUM(COALESCE(missing_qualifying_awards.num_missing_qualifying_awards, 0)) -- If RAS was not awarded at a completed DCMP event, then an extra point spot is available
+            + SUM(COALESCE(bonus_point_spots.num_missing_qualifying_awards, 0)) -- If RAS was not awarded at a completed DCMP event, then an extra point spot is available
         AS wcmp_spots
     FROM district_lookup
     LEFT JOIN bonus_point_spots ON district_lookup.district_key = bonus_point_spots.district_key
-    LEFT JOIN missing_qualifying_awards ON district_lookup.district_key = missing_qualifying_awards.district_key
     GROUP BY
         district_lookup.district_key
 )
